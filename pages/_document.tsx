597f85a6f94ae82551228a2cbcf0e49fd5f0e768
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2023-12-13 09:48:19 +0100

    feat: add guest states for navbar & login overlay (#3)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
 export default function Document() {
   return (
     <Html>
       <Head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
-      <body>
+      <body className="bg-theme-secondary-50">
         <Main />
         <NextScript />
       </body>
