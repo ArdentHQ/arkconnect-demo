88acd53607bd3227d9054cba95323683ff2844e4
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-12-15 11:30:59 -0600

    default input

--- src/app/components/Input/Input.tsx
+++ src/app/components/Input/Input.tsx
@@ -1,11 +1,18 @@
+import classNames from "classnames";
 import React, { useRef, forwardRef, useEffect } from "react";
+
+type InputVariant = "default" | "danger";
 
 interface InputProperties extends React.HTMLProps<HTMLInputElement> {
   isFocused?: boolean;
+  variant?: InputVariant;
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProperties>(
-  ({ isFocused, ...properties }: InputProperties, reference) => {
+  (
+    { isFocused = false, variant = "default", ...properties }: InputProperties,
+    reference,
+  ) => {
     const focusReference = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
@@ -15,7 +22,19 @@ export const Input = forwardRef<HTMLInputElement, InputProperties>(
     }, [focusReference, isFocused]);
 
     return (
-      <input ref={isFocused ? focusReference : reference} {...properties} />
+      <input
+        className={classNames(
+          "placeholder-theme-gray-400 px-4 py-3 rounded-lg ring-1 ring-inset text-black",
+          "enabled:bg-white enabled:ring-theme-gray-400 enabled:active:ring-black enabled:focus:ring-black",
+          "disabled:ring-theme-gray-200",
+          "focus:outline-none",
+          {
+            "": true,
+          },
+        )}
+        ref={isFocused ? focusReference : reference}
+        {...properties}
+      />
     );
   },
 );
