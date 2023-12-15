5eaf8abb479c615752439a020c561011c4554353
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-12-15 13:53:44 -0600

    i18n

--- src/domains/transactions/components/SendModal/SendModal.tsx
+++ src/domains/transactions/components/SendModal/SendModal.tsx
@@ -1,7 +1,9 @@
+import assert from "assert";
 import { useTranslation } from "next-i18next";
 import { Dialog } from "@/app/components/Dialog";
 import { InputGroup } from "@/app/components/InputGroup";
 import { Input } from "@/app/components/Input";
+import { useWallet } from "@/app/hooks";
 
 export const SendModal = ({
   show,
@@ -12,6 +14,10 @@ export const SendModal = ({
 }) => {
   const { t } = useTranslation("transactions");
 
+  const { wallet } = useWallet();
+
+  assert(wallet);
+
   return (
     <Dialog
       show={show}
@@ -21,41 +27,37 @@ export const SendModal = ({
       title={t("SEND_ARK")}
     >
       <div className="flex flex-col space-y-4">
-        <InputGroup label="Sender">
+        <InputGroup label={t("SENDER")}>
           <div className="relative inline-flex items-center flex-row-reverse">
             <span className="block px-2 py-1 rounded-lg bg-theme-gray-300 text-black text-sm font-medium absolute mr-3 pointer-events-none">
-              {"You"}
+              {t("YOU", { ns: "common" })}
             </span>
 
-            <Input
-              className="w-full pr-16"
-              value="AGURf8NdhVj4fvz5mK39qMgPoLGPgJNBrc"
-              disabled
-            />
+            <Input className="w-full pr-16" value={wallet.address} disabled />
           </div>
         </InputGroup>
 
         <InputGroup
-          label="Recipient"
+          label={t("RECIPIENT")}
           variant="error"
-          help="Recipient address is invalid"
+          help={t("RECIPIENT_INVALID_ADDRESS")}
         >
-          <Input placeholder="Enter Recipient" />
+          <Input placeholder={t("ENTER_RECIPIENT")} />
         </InputGroup>
 
         <InputGroup
           label={
             <span className="inline-flex justify-between flex-1 w-full">
-              <span>{"Amount"}</span>
+              <span>{t("AMOUNT", { ns: "common" })}</span>
 
               <span>
-                <span className="text-theme-gray-400">{"Available"}</span>{" "}
-                <span>{"300 ARK"}</span>
+                <span className="text-theme-gray-400">{t("AVAILABLE")}</span>{" "}
+                <span>{`${wallet.balance} ARK`}</span>
               </span>
             </span>
           }
           variant="error"
-          help="The balance is too low"
+          help={t("BALANCE_TOO_LOW")}
         >
           <Input placeholder="Enter Amount" />
         </InputGroup>
