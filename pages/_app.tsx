a8a342b1a5febfd2d84218db8498a044c5df6b57
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2023-12-15 17:18:34 +0100

    fix: apply fonts globally (#11)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,29 +2,24 @@ import "@/app/styles/globals.css";
 
 import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
-import { DM_Sans } from "next/font/google";
-
 import { useState } from "react";
+
 import {
   ReactQueryClient,
   QueryClientProvider,
 } from "@/app/contexts/ReactQuery";
-
-const dmSans = DM_Sans({
-  weight: ["300", "400", "500", "700"],
-  subsets: ["latin"],
-  variable: "--font-dm-sans",
-});
+import { AppFont } from "@/app/components/AppFont";
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [queryClient] = useState(() => ReactQueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className={`${dmSans.variable} font-sans`}>
+    <>
+      <AppFont />
+      <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
-      </div>
-    </QueryClientProvider>
+      </QueryClientProvider>
+    </>
   );
 };
 
