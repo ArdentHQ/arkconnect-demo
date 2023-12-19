dd9df0a668327fa40bb8efc884d785a0284aa4ea
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-12-19 02:48:40 -0600

    feat: add favicon (#26)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,27 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#01b86c" />
+        <meta name="msapplication-TileColor" content="#01b86c" />
+        <meta name="theme-color" content="#01b86c" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
       <body className="bg-theme-gray-50">
