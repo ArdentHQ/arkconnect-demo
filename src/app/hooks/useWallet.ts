e404b70b1200368ea0ad58ec0172fb7c3d5bac8d
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2023-12-20 16:34:16 +0100

    fix: persist selected network when disconnected (#52)

--- src/app/hooks/useWallet.ts
+++ src/app/hooks/useWallet.ts
@@ -88,7 +88,9 @@ export const useWallet = (): UseWalletReturnType => {
       if (!isTruthy(isConnected)) {
         return {
           wallet: {
-            ...existingState,
+            // @TODO: cleanup queryFn logic & types.
+            // @ts-ignore
+            ...existingState.wallet,
             address: undefined,
             coin: undefined,
           },
@@ -142,14 +144,12 @@ export const useWallet = (): UseWalletReturnType => {
     wallet: data.wallet,
     connect: async () => {
       if (!isTruthy(data) || !isTruthy(data.extension)) {
-        console.log("case 1");
-        // TODO TBD
+        // @TODO TBD
         return;
       }
 
       if (data.isConnected) {
-        console.log("case 2");
-        // TODO TBD
+        // @TODO TBD
         return;
       }
 
