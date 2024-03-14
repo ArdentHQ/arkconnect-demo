11caf19bfc096b7495590564c7e134a86c78dd9c
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-03-14 16:54:10 -0500

    feat: dark mode for labels

--- src/app/components/Label/hooks/useLabelClasses.ts
+++ src/app/components/Label/hooks/useLabelClasses.ts
@@ -4,11 +4,11 @@ export const useLabelClasses = ({ variant }: { variant: LabelVariant }) => {
   const base = "rounded-[0.25rem] text-center min-w-[2.5rem] inline-block";
   const padding = "px-1 py-[0.188rem]";
   const colors = {
-    info: "bg-theme-gray-100 text-theme-gray-500 font-medium leading-[125%]",
+    info: "bg-theme-gray-100 text-theme-gray-500 font-medium leading-[125%] dark:bg-theme-gray-700 dark:text-theme-gray-300",
     warning:
-      "bg-theme-warning-100 text-theme-warning-600 font-medium leading-[125%]",
+      "bg-theme-warning-100 text-theme-warning-600 font-medium leading-[125%] dark:bg-transparent dark:text-[#F39B9B] dark:outline dark:outline-[#AA6868]",
     success:
-      "bg-theme-primary-100 text-theme-primary-700 font-medium leading-[125%]",
+      "bg-theme-primary-100 text-theme-primary-700 font-medium leading-[125%] dark:bg-transparent dark:text-theme-primary-600 dark:outline dark:outline-theme-primary-600",
     danger: "bg-theme-error-50 text-theme-error-600 font-medium leading-[125%]",
   };
 
