d270daa0148bd8c7445578a4c6c6e584af8752b1
Patricio Marroquin <55117912+patricio0312rev@users.noreply.github.com>
2024-03-14 10:31:53 -0500

    feat: dark mode initial config (#87)

---
+++ src/app/contexts/useDarkModeContext.tsx
@@ -0,0 +1,53 @@
+import { createContext, useState, useEffect, useContext } from "react";
+
+interface DarkModeContextType {
+  darkMode: boolean;
+  toggleDarkMode: () => void;
+}
+
+export const DarkModeContext = createContext<DarkModeContextType | undefined>({
+  darkMode: false,
+  toggleDarkMode: () => {},
+});
+
+export const DarkModeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem("darkMode");
+    if (isDarkMode) {
+      setDarkMode(!!JSON.parse(isDarkMode));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    localStorage.setItem("darkMode", JSON.stringify(!darkMode));
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export const useDarkMode = () => {
+  const context = useContext(DarkModeContext);
+
+  if (context === undefined) {
+    throw new Error(
+      "useDarkModeContext must be within ArkConnectContext.Provider",
+    );
+  }
+
+  return context;
+};
