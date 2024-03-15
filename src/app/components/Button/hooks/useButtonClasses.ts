ae5a5a79a921ba912977209062f5b3df500c58de
Patricio Marroquin <55117912+patricio0312rev@users.noreply.github.com>
2024-03-15 04:37:04 -0500

    refactor: welcome page dark mode (#90)

--- src/app/components/Button/hooks/useButtonClasses.ts
+++ src/app/components/Button/hooks/useButtonClasses.ts
@@ -22,7 +22,8 @@ export const useButtonClasses = ({
   hover: string;
 } => {
   const colors = {
-    primary: "bg-theme-primary-700 text-white border-theme-primary-700",
+    primary:
+      "bg-theme-primary-700 text-white border-theme-primary-700 dark:bg-theme-primary-600 dark:border-theme-primary-600",
     secondary: "border border-white bg-white text-theme-primary-700",
     "secondary-bordered": "border border-white",
     transparent:
@@ -47,7 +48,7 @@ export const useButtonClasses = ({
   } else {
     disabledClass = {
       primary:
-        "disabled:bg-theme-gray-100 disabled:border-theme-gray-100 disabled:text-theme-gray-400",
+        "disabled:bg-theme-gray-100 disabled:border-theme-gray-100 disabled:text-theme-gray-400 dark:disabled:bg-theme-gray-600 dark:disabled:border-theme-gray-600 dark:disabled:text-theme-gray-300",
       secondary: "disabled:text-theme-gray-300 disabled:border-theme-gray-100",
       "secondary-bordered":
         "disabled:text-theme-gray-300 disabled:border-theme-gray-300",
@@ -63,7 +64,7 @@ export const useButtonClasses = ({
 
   const hover =
     hoverClassName ??
-    "hover:bg-theme-primary-600 hover:text-white hover:border-theme-primary-600";
+    "hover:bg-theme-primary-600 hover:text-white hover:border-theme-primary-600 dark:hover:bg-theme-primary-700 dark:hover:border-theme-primary-700";
 
   return {
     disabled: busy
