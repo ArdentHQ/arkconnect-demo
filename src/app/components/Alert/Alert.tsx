cdb79fd5a00b10cad866983a074778cea869b933
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-05-24 12:58:47 -0600

    color adjustments

--- src/app/components/Alert/Alert.tsx
+++ src/app/components/Alert/Alert.tsx
@@ -6,7 +6,7 @@ type AlertProperties = HTMLAttributes<HTMLElement> & {
 
 const AlertSuccess = (properties: HTMLAttributes<HTMLElement>): JSX.Element => (
   <div
-    className="border-t border-theme-primary-600 bg-theme-primary-100 text-theme-primary-700 text-sm px-8 py-3 dark:bg-theme-primary-600/20 dark:text-theme-primary-600 dark:border-theme-primary-600"
+    className="border-t border-theme-primary-600 bg-theme-primary-100 text-theme-primary-700 text-sm px-8 py-3 dark:bg-theme-dark-primary-600/20 dark:text-theme-dark-primary-600 dark:border-theme-dark-primary-600"
     {...properties}
   />
 );
