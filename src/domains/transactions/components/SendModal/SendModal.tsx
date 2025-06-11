3aa79689536a88279fcff2b69a4b02fe2af7774b
Shahin Safaraliyev <shahin@ardenthq.com>
2025-06-11 16:14:08 +0400

    feat: implement sending transfers through MetaMask

--- src/domains/transactions/components/SendModal/SendModal.tsx
+++ src/domains/transactions/components/SendModal/SendModal.tsx
@@ -133,7 +133,11 @@ export const SendModal = ({
               {t("YOU", { ns: "common" })}
             </span>
 
-            <Input className="w-full pr-16 truncate" value={wallet.address} disabled />
+            <Input
+              className="w-full pr-16 truncate"
+              value={wallet.address}
+              disabled
+            />
           </div>
         </InputGroup>
 
